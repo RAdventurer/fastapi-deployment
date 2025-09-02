5f77d36367266defdd969a2df0e740c6569375c5
RAdvanture <rouaibia.ro@gmail.com>
2025-09-02 04:02:32 +0100

    workflowJJJJ commit

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ app = FastAPI()
 # Root route
 @app.get("/")
 def root():
-    return {"message": "Welcome to the root route!"}
+    return {"message": "Welcome to the BRANCH root route!"}
 
 # Route to display info for items
 @app.get("/items/")
 def items():
-    return {"message": "Welcome to the items route!"}
+    return {"message": "Welcome to the BRANCH items route!"}
 
 # Route to display info for updating items
 @app.get("/items/update/")
 def update_item():
-    return {"message": "Welcome to the update item route!"}
+    return {"message": "Welcome to the BRANCH update item route!"}
